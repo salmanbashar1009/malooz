bf6b9166a8eda192e45a02337d7bd178f317e20a
salmanbashar1009 <salmanbashar101@gmail.com>
2025-07-01 22:07:22 +0600

    app_colors.dart file added

---
+++ lib/core/constant/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+  /// App default color
+  static const Color primaryColor = Color(0xFFB8EB00);
+
+  /// body text color
+  static const Color bodyTextColor = Color(0xFFCACACB);
+
+
+  ///other
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF717374);
+
+  /// Colors with opacity
+  static const Color white40 = Color.fromRGBO(255, 255, 255, 0.4); /// used for body text
+  static const Color white10 = Color.fromRGBO(246, 246, 246, 0.1); /// used for body text
+
+
+}
