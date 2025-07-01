58d56b9055e73493baedec67877473d06c3109e0
salmanbashar1009 <salmanbashar101@gmail.com>
2025-07-01 23:32:22 +0600

    app_theme.dart file added

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../constant/app_colors.dart';
+
+class AppTheme {
+  static ThemeData dark = ThemeData(
+    scaffoldBackgroundColor: AppColors.black, // or AppColors.darkBackground
+    colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: AppColors.primaryColor,
+      onPrimary: AppColors.black,
+      secondary: AppColors.black,
+      onSecondary: AppColors.white40,
+      error: Colors.red,
+      onError: AppColors.black,
+      surface: AppColors.black ,
+      onSurface: AppColors.white,
+    ),
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+    ),
+    textTheme: AppTextTheme.darkTextTheme,
+    inputDecorationTheme: AppInputDecorationTheme.dark,
+  );
+
+}
