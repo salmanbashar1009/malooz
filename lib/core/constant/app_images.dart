f7641b6b94b190e33ad968b608f886f6aa7ee0ba
salmanbashar1009 <salmanbashar101@gmail.com>
2025-07-01 22:11:33 +0600

    app_images.dart added

---
+++ lib/core/constant/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages{
+  /// App logo
+  static const String appLogo = "assets/images/onboarding/logo.png";
+
+  /// Onboarding images
+  static const String onboardingBg = "assets/images/onboarding/bg.png";
+}
