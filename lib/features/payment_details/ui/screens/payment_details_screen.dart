b9792effea29b5b468a2697cddce8d58f1321548
salmanbashar1009 <salmanbashar101@gmail.com>
2025-07-11 12:20:34 +0600

    payment screen ui complete

--- lib/features/payment_details/ui/screens/payment_details_screen.dart
+++ lib/features/payment_details/ui/screens/payment_details_screen.dart
@@ -6,69 +6,157 @@ import 'package:malooz/core/constant/app_padding.dart';
 import 'package:malooz/core/utls/utls.dart';
 import 'package:malooz/features/payment_details/ui/widgets/select_payment_method.dart';
 
-import '../../../../core/constant/app_images.dart';
-
 class PaymentDetailsScreen extends StatelessWidget {
   const PaymentDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final textTheme = GoogleFonts.urbanist(
-      fontSize: 14,
-      fontWeight: FontWeight.w500,
-      color: Colors.white70
-    );
+        fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white70);
     return Scaffold(
-      appBar: Utils.appBar(context: context,title: 'Payment Details'),
-      body: Padding(
-        padding: AppPadding.horizontal16,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16,),
-            Text("Please provide the payment details below",style: textTheme,),
-            const SizedBox(height: 13,),
-            SelectPaymentMethod(),
-            const SizedBox(height: 13,),
-            Text("Card Details:",style: textTheme.copyWith(
-              fontWeight: FontWeight.w600,
-              fontSize: 16,
-              color: AppColors.white
-            ),),
-            const SizedBox(height: 12,),
-            TextFormField(
-              textAlignVertical: TextAlignVertical.center, // Aligns the input text and hintText vertically
-              decoration: InputDecoration(
-                hintText: 'XXXX - XXXX - XXXX - XXXX',
-                hintStyle: textTheme.copyWith(
-                  fontWeight: FontWeight.w400,
-                ),
-                suffixIcon: Padding(
-                  padding: const EdgeInsets.only(right: 12.0), // Adjust right padding as needed
-                  child: ImageIcon(
-                    AssetImage(AppIcons.visa),
-                    color: AppColors.primaryColor,
-                    size: 24,
-                  ),
-                ),
-                suffixIconConstraints: const BoxConstraints(
-                  minHeight: 24, // Should be at least the size of your icon
-                  minWidth: 24,  // Should be at least the size of your icon
-                ),
-                contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0), // Adjust padding as needed
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: AppColors.white6)),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: AppColors.white6)),
+      appBar: Utils.appBar(context: context, title: 'Payment Details'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: AppPadding.horizontal16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 16,
               ),
-            )
-
-
-
-          ],
+              Text(
+                "Please provide the payment details below",
+                style: textTheme,
+              ),
+              const SizedBox(
+                height: 13,
+              ),
+              SelectPaymentMethod(),
+              const SizedBox(
+                height: 13,
+              ),
+        
+              /// Card Details Section
+              Text(
+                "Card Details:",
+                style: textTheme.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: AppColors.white),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              buildTextFormField(
+                  hintText: 'XXXX - XXXX - XXXX - XXXX',
+                  suffixIcon: Padding(
+                    padding: const EdgeInsets.only(right: 12.0),
+                    // Adjust right padding as needed
+                    child: ImageIcon(
+                      AssetImage(AppIcons.visa),
+                      color: AppColors.primaryColor,
+                      size: 24,
+                    ),
+                  )),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: buildTextFormField(hintText: 'Expiry Date'),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: buildTextFormField(hintText: 'CVV'),
+                  )
+                ],
+              ),
+              const SizedBox(height: 24,),
+        
+              /// Card holder Details Section
+              Text(
+                "Card Holder Details:",
+                style: textTheme.copyWith(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: AppColors.white),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              buildTextFormField(hintText: 'Card Holder Name'),
+              const SizedBox(
+                height: 8,
+              ),
+              buildTextFormField(hintText: 'Billing address:'),
+              const SizedBox(
+                height: 8,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: buildTextFormField(hintText: 'City'),
+                  ),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: buildTextFormField(hintText: 'Country'),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 24,
+              ),
+              buildTextFormField(hintText: 'If any Note:',maxLine: 3),
+              const SizedBox(
+                height: 40,
+              ),
+              Utils.primaryButton(title: "Continue", onPressed: (){}),
+              const SizedBox(height: 32,)
+        
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  TextFormField buildTextFormField(
+      {String? hintText,
+      TextEditingController? controller,
+        int? maxLine,
+      Widget? suffixIcon}) {
+    final textTheme = GoogleFonts.urbanist(
+        fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white70);
+    return TextFormField(
+      controller: controller,
+      maxLines: maxLine,
+      textAlignVertical: TextAlignVertical.center,
+      // Aligns the input text and hintText vertically
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: textTheme.copyWith(
+          fontWeight: FontWeight.w400,
+          color: AppColors.white60
+        ),
+        suffixIcon: suffixIcon,
+        suffixIconConstraints: const BoxConstraints(
+          minHeight: 24, // Should be at least the size of your icon
+          minWidth: 24, // Should be at least the size of your icon
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+        // Adjust padding as needed
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.white6)),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: AppColors.white6)),
       ),
     );
   }
