e4c4ae5d1e05bdb81b376408d1288be5073e24b2
salmanbashar1009 <salmanbashar101@gmail.com>
2025-07-14 00:34:21 +0600

    faq screen ui updated

--- lib/features/faq/ui/screens/faq_screen.dart
+++ lib/features/faq/ui/screens/faq_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:malooz/core/constant/app_colors.dart';
+import 'package:malooz/core/constant/app_padding.dart';
+import 'package:malooz/core/utls/utls.dart';
 
 import '../../cubit/faq_cubit.dart';
 
@@ -7,7 +10,7 @@ class FAQScreen extends StatelessWidget {
   final List<Map<String, String>> faqItems = [
     {
       'question': 'What is Lorem Ipsum?',
-      'answer': '',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
     },
     {
       'question': 'Why do we use Lorem Ipsum?',
@@ -16,19 +19,35 @@ class FAQScreen extends StatelessWidget {
     },
     {
       'question': 'What is Lorem Ipsum?',
-      'answer': '',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
     },
     {
       'question': 'What is Lorem Ipsum?',
-      'answer': '',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
     },
     {
       'question': 'What is Lorem Ipsum?',
-      'answer': '',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
     },
     {
       'question': 'What is Lorem Ipsum?',
-      'answer': '',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
+    },
+    {
+      'question': 'What is Lorem Ipsum?',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
+    },
+    {
+      'question': 'What is Lorem Ipsum?',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
+    },
+    {
+      'question': 'What is Lorem Ipsum?',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
+    },
+    {
+      'question': 'What is Lorem Ipsum?',
+      'answer': 'It is a long established fact that a reader will be distracted by the readable content of a page ',
     },
   ];
 
@@ -39,94 +58,72 @@ class FAQScreen extends StatelessWidget {
     return BlocProvider(
       create: (_) => FaqCubit(),
       child: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            children: [
-              // Custom AppBar
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12),
-                child: Row(
-                  children: [
-                    Icon(Icons.arrow_back, color: Colors.white),
-                    SizedBox(width: 16),
-                    Text(
-                      'FAQ',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              // FAQ List
-              Expanded(
-                child: BlocBuilder<FaqCubit, int?>(
-                  builder: (context, expandedIndex) {
-                    return ListView.builder(
-                      itemCount: faqItems.length,
-                      itemBuilder: (context, index) {
-                        final isExpanded = expandedIndex == index;
-                        return GestureDetector(
-                          onTap: () => context.read<FaqCubit>().toggle(index),
-                          child: AnimatedContainer(
-                            duration: Duration(milliseconds: 300),
-                            curve: Curves.easeInOut,
-                            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                            decoration: BoxDecoration(
-                              border: Border(
-                                top: BorderSide(color: Colors.grey.shade800),
-                                bottom: BorderSide(color: Colors.grey.shade800),
-                              ),
-                              color: Colors.black,
+        appBar: Utils.appBar(context: context,title: 'FAQ'),
+        body: Column(
+          children: [
+            const SizedBox(height: 70),
+            Expanded(
+              child: BlocBuilder<FaqCubit, int?>(
+                builder: (context, expandedIndex) {
+                  return ListView.builder(
+                    itemCount: faqItems.length,
+                    itemBuilder: (context, index) {
+                      final isExpanded = expandedIndex == index;
+                      return GestureDetector(
+                        onTap: () => context.read<FaqCubit>().toggle(index),
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                          padding: AppPadding.padding16,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              top: BorderSide(color: AppColors.white50),
+                              bottom:  BorderSide(color:  index == faqItems.length - 1 ? AppColors.white50 : Colors.transparent),
                             ),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      child: Text(
-                                        faqItems[index]['question']!,
-                                        style: TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                          fontSize: 16,
-                                        ),
-                                      ),
-                                    ),
-                                    Icon(
-                                      isExpanded ? Icons.expand_less : Icons.expand_more,
-                                      color: Colors.white,
-                                    ),
-                                  ],
-                                ),
-                                if (isExpanded && faqItems[index]['answer']!.isNotEmpty)
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 12),
+                            // color: Colors.black,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
                                     child: Text(
-                                      faqItems[index]['answer']!,
-                                      style: TextStyle(
-                                        color: Colors.white70,
-                                        fontSize: 14,
+                                      faqItems[index]['question']!,
+                                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                                        fontWeight: FontWeight.w900,
+                                        color: Colors.white,
                                       ),
                                     ),
                                   ),
-                              ],
-                            ),
+                                  Icon(
+                                    isExpanded ? Icons.expand_less : Icons.expand_more,
+                                    color: Colors.white,
+                                  ),
+                                ],
+                              ),
+                              if (isExpanded && faqItems[index]['answer']!.isNotEmpty)
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 12),
+                                  child: Text(
+                                    faqItems[index]['answer']!,
+                                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400
+                                    ),
+                                  ),
+                                ),
+                            ],
                           ),
-                        );
-                      },
-                    );
-                  },
-                ),
+                        ),
+                      );
+                    },
+                  );
+                },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
